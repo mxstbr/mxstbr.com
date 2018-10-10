901dc804d4734fdb3298868d377d3a75cb60340e
Maximilian Stoiber <contact@mxstbr.com>
2018-10-10 16:41:24 +0200

    Responsive Card

--- components/Card.js
+++ components/Card.js
@@ -5,7 +5,7 @@ import Text from "./Text";
 import BoxShadow from "./BoxShadow";
 
 const Title = styled(props => (
-  <H3 pl={4} pr={4} pt={4} my={0} lineHeight={1} {...props} />
+  <H3 pl={[3, 4]} pr={[3, 4]} pt={[3, 4]} my={0} lineHeight={1} {...props} />
 ))``;
 
 const BaseCard = styled(Card)`
@@ -31,8 +31,8 @@ C.Body = props => (
     color="#666"
     fontSize={2}
     lineHeight={1.25}
-    px={4}
-    mb={4}
+    px={[3, 4]}
+    mb={[3, 4]}
     mt={2}
     {...props}
   />
@@ -41,7 +41,14 @@ C.Image = props => (
   <Image {...props} css={{ borderRadius: "5px 5px 0 0", ...props.css }} />
 );
 C.FinePrint = props => (
-  <Text color="#666" pb={4} pl={4} pr={4} fontSize={1} {...props} />
+  <Text
+    color="#666"
+    pb={[3, 4]}
+    pl={[3, 4]}
+    pr={[3, 4]}
+    fontSize={1}
+    {...props}
+  />
 );
 
 export default C;
