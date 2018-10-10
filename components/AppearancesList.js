d764133c10ef5abcb69e90b2ceac85328759ce40
Maximilian Stoiber <contact@mxstbr.com>
2018-10-10 13:32:12 +0200

    Fit appearances list to grid

--- components/AppearancesList.js
+++ components/AppearancesList.js
@@ -4,19 +4,13 @@ import { H3 } from "./Heading";
 import Appearance from "./Appearance";
 
 export default ({ appearances }) => (
-  <Flex flexDirection="column" width={734 - 16}>
+  <Flex flexDirection="column" width={0.5}>
     {appearances.map((appearance, i) => (
       <React.Fragment key={appearance.title + appearance.site}>
         {!appearances[i - 1] ||
         appearances[i - 1].date.getFullYear() !==
           appearance.date.getFullYear() ? (
-          <Flex
-            flexDirection="row"
-            alignItems="center"
-            width={734 - 16}
-            mb={2}
-            mt={3}
-          >
+          <Flex flexDirection="row" alignItems="center" width={1} mb={2} mt={3}>
             <H3 mr={3} width={50}>
               {appearance.date.getFullYear()}
             </H3>
