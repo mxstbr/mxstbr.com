a5e48137bfec810a60e59034c9fcb12ff91b0517
Maximilian Stoiber <contact@mxstbr.com>
2018-10-10 09:47:00 +0200

    Setup svg import

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
-const withMDX = require('@zeit/next-mdx')()
+const withMDX = require("@zeit/next-mdx")();
 
 module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'mdx'] 
-})
+  pageExtensions: ["js", "jsx", "mdx"],
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            icon: true
+          }
+        }
+      ]
+    });
+    return config;
+  }
+});
