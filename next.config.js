c6abde42de47cf249ca199492e34a5c74d85d540
Maximilian Stoiber <contact@mxstbr.com>
2018-10-11 11:37:38 +0200

    Copy now.json and favicon to static out/ export dir

--- next.config.js
+++ next.config.js
@@ -1,7 +1,24 @@
 const withMDX = require("@zeit/next-mdx")();
+const fs = require("fs");
+const { join } = require("path");
+const { promisify } = require("util");
+const copyFile = promisify(fs.copyFile);
+
+const staticFilesToCopy = ["now.json", "favicon.ico"];
 
 module.exports = withMDX({
   pageExtensions: ["js", "jsx", "mdx"],
+  exportPathMap: async function(
+    defaultPathMap,
+    { dir, outDir, distDir, buildId }
+  ) {
+    await Promise.all(
+      staticFilesToCopy.map(file =>
+        copyFile(join(dir, file), join(outDir, file))
+      )
+    );
+    return defaultPathMap;
+  },
   webpack(config, options) {
     config.module.rules.push({
       test: /.svg$/,
