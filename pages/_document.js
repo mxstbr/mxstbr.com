6696d679387c55bfb8959e4a96d9b02eda97badc
Maximilian Stoiber <contact@mxstbr.com>
2018-10-11 11:36:58 +0200

    Add lang attr to <html>

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <html>
+      <html lang="en">
         <Head>
           <meta charSet="UTF-8" />
           <meta
