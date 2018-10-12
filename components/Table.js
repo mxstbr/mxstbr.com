2aa1a517987cd5e8f5d5a3e5fa89e84632901f7d
Maximilian Stoiber <contact@mxstbr.com>
2018-10-12 10:23:32 +0200

    Create <Table /> components

---
+++ components/Table.js
@@ -0,0 +1,76 @@
+import React from "react";
+import styled, { css } from "styled-components";
+import ConditionalWrap from "conditional-wrap";
+import {
+  Flex,
+  Box,
+  Heading as RebassHeading,
+  Link as RebassLink
+} from "rebass";
+import Link from "../components/Link";
+
+import BoxShadow from "./BoxShadow";
+
+const Row = styled(Flex)`
+  &:hover ${RebassHeading} {
+    text-decoration: underline;
+  }
+`;
+
+const Td = styled.td`
+  ${props => props.textAlign && `text-align: ${props.textAlign}`};
+  ${props => props.padding && `padding: ${props.padding}`};
+
+  ${RebassLink} {
+    display: block;
+    padding: 8px 0;
+
+    &:hover {
+      text-decoration: none;
+    }
+  }
+`;
+
+const Cell = props => (
+  <Td textAlign={props.textAlign} padding={props.padding}>
+    <ConditionalWrap
+      condition={!!props.href}
+      wrap={children => <Link href={props.href}>{children}</Link>}
+    >
+      {props.children || null}
+    </ConditionalWrap>
+  </Td>
+);
+
+const RowLink = styled(Link)`
+  border-bottom: 1px solid #eee;
+
+  &:last-of-type {
+    border-bottom: none;
+  }
+
+  &:hover ${RebassHeading} {
+    text-decoration: underline;
+  }
+`;
+
+const Table = props => (
+  <Flex width={1}>
+    <BoxShadow hoverShadow="none" width={1}>
+      <Flex
+        flexDirection="column"
+        bg="#fff"
+        css={{ borderRadius: "5px", border: "1px solid #eee" }}
+        width={1}
+      >
+        {props.rows.map(row => (
+          <RowLink py={3} px={3} key={row.id} href={row.href} underline={false}>
+            <Flex>{props.render(row)}</Flex>
+          </RowLink>
+        ))}
+      </Flex>
+    </BoxShadow>
+  </Flex>
+);
+
+export default Table;
--- pages/oss.js
+++ pages/oss.js
@@ -1,108 +1,33 @@
 import React from "react";
 import styled from "styled-components";
-import {
-  Flex,
-  Heading as RebassHeading,
-  Link as RebassLink,
-  Box
-} from "rebass";
-import ConditionalWrap from "conditional-wrap";
-import projects from "../open-source-projects";
-import { ListItem } from "../components/Lists";
+import { Flex } from "rebass";
 import Text from "../components/Text";
 import Paragraph from "../components/Paragraph";
 import Link from "../components/Link";
 import Head from "../components/Head";
 import Heading, { H2, H3 } from "../components/Heading";
-import OSSProject from "../components/OpenSourceProjectCard";
-import CardGrid from "../components/CardGrid";
-import BoxShadow from "../components/BoxShadow";
 import Main from "../components/Main";
 import PageHeader from "../components/PageHeader";
+import Table from "../components/Table";
 
-const Row = styled(Flex)`
-  &:hover ${RebassHeading} {
-    text-decoration: underline;
-  }
-`;
+import projects from "../open-source-projects";
 
-const Td = styled.td`
-  ${props => props.textAlign && `text-align: ${props.textAlign}`};
-  ${props => props.padding && `padding: ${props.padding}`};
-
-  ${RebassLink} {
-    display: block;
-    padding: 8px 0;
-
-    &:hover {
-      text-decoration: none;
-    }
-  }
-`;
-
-const Cell = props => (
-  <Td textAlign={props.textAlign} padding={props.padding}>
-    <ConditionalWrap
-      condition={!!props.href}
-      wrap={children => <Link href={props.href}>{children}</Link>}
-    >
-      {props.children || null}
-    </ConditionalWrap>
-  </Td>
-);
-
-const RowLink = styled(Link)`
-  border-bottom: 1px solid #eee;
-
-  &:last-of-type {
-    border-bottom: none;
-  }
-
-  &:hover ${RebassHeading} {
-    text-decoration: underline;
-  }
-`;
-
-const Table = props => (
+const OpenSourceProjectTableRow = ({ project }) => (
   <>
-    <BoxShadow hoverShadow="none">
-      <Flex
-        flexDirection="column"
-        bg="#fff"
-        css={{ borderRadius: "5px", border: "1px solid #eee" }}
-      >
-        {props.projects.map(project => (
-          <RowLink
-            py={3}
-            px={3}
-            key={project.repo}
-            href={`https://github.com/${project.repo}`}
-            underline={false}
-          >
-            <Flex>
-              <Flex width={0.2} alignItems="center" height={1}>
-                <Heading as="h4" fontSize={2} alignSelf="center">
-                  {project.name}
-                </Heading>
-              </Flex>
-              <Flex alignItems="center" width={0.75} px={3}>
-                <Text color="#666">{project.description}</Text>
-              </Flex>
-              <Flex alignItems="center" justifyContent="flex-end" width={0.06}>
-                <Text textAlign="right" color="#666" fontSize={1}>
-                  {project.stars.toLocaleString()}
-                  &nbsp;★
-                </Text>
-              </Flex>
-            </Flex>
-          </RowLink>
-        ))}
-      </Flex>
-    </BoxShadow>
-    <Text color="#666" mt={3} fontSize={2}>
-      Total: {props.projects.reduce((total, { stars }) => total + stars, 0)}
-      &nbsp;★
-    </Text>
+    <Flex width={0.2} alignItems="center" height={1}>
+      <Heading as="h4" fontSize={2} alignSelf="center">
+        {project.name}
+      </Heading>
+    </Flex>
+    <Flex alignItems="center" width={0.75} px={3}>
+      <Text color="#666">{project.description}</Text>
+    </Flex>
+    <Flex alignItems="center" justifyContent="flex-end" width={0.06}>
+      <Text textAlign="right" color="#666" fontSize={1}>
+        {project.stars.toLocaleString()}
+        &nbsp;★
+      </Text>
+    </Flex>
   </>
 );
 
@@ -126,10 +51,23 @@ export default () => (
       </Paragraph>
     </PageHeader>
     <Table
-      projects={projects
+      rows={projects
         .filter(p => p.active !== false)
-        .sort((a, b) => b.stars - a.stars)}
+        .sort((a, b) => b.stars - a.stars)
+        .map(p => ({
+          ...p,
+          id: p.repo,
+          href: `https://github.com/${p.repo}`
+        }))}
+      render={row => <OpenSourceProjectTableRow project={row} />}
     />
+    <Text color="#666" mt={3} fontSize={2}>
+      Total:{" "}
+      {projects
+        .filter(p => p.active !== false)
+        .reduce((total, { stars }) => total + stars, 0)}
+      &nbsp;★
+    </Text>
     <H2 my={null} mt={4} mb={3} fontSize={3}>
       Past Open Source Projects
     </H2>
@@ -138,9 +76,15 @@ export default () => (
       them or they are archived.
     </Paragraph>
     <Table
-      projects={projects
+      rows={projects
         .filter(p => p.active === false)
-        .sort((a, b) => b.stars - a.stars)}
+        .sort((a, b) => b.stars - a.stars)
+        .map(p => ({
+          ...p,
+          id: p.repo,
+          href: `https://github.com/${p.repo}`
+        }))}
+      render={row => <OpenSourceProjectTableRow project={row} />}
     />
   </Main>
 );
