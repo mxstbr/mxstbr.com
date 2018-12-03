9c51a1cf9a6a8b3a240bcfef4ba0c6b1a5ab40ec
Max Stoiber <mxstbr@github.com>
2018-12-03 13:31:56 +0100

    Upgrade all deps

--- components/Icon.js
+++ components/Icon.js
@@ -2,11 +2,11 @@ import React from "react";
 import styled from "styled-components";
 import { Box, type RebassProps } from "rebass";
 
-export default styled(Box).attrs({
+export default styled(Box).attrs(props => ({
   ml: 1,
-  css: props => ({
+  css: {
     verticalAlign: "middle",
     display: "inline-block",
     ...props.css
-  })
-})``;
+  }
+}))``;
