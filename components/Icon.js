6aeffc9e957cd6526e2a09ad0657a0572720289f
Max Stoiber <mxstbr@github.com>
2018-12-03 13:38:43 +0100

    Revert "Upgrade all deps"
    This reverts commit ecc15fc1884939e8a05f8a9e6b584e854f9915ec.

--- components/Icon.js
+++ components/Icon.js
@@ -2,11 +2,11 @@ import React from "react";
 import styled from "styled-components";
 import { Box, type RebassProps } from "rebass";
 
-export default styled(Box).attrs(props => ({
+export default styled(Box).attrs({
   ml: 1,
-  css: {
+  css: props => ({
     verticalAlign: "middle",
     display: "inline-block",
     ...props.css
-  }
-}))``;
+  })
+})``;
