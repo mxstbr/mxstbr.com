f68cb7afe531c7dc2395a8399158e2818b15b926
Max Stoiber <mxstbr@github.com>
2019-01-21 10:39:19 +0100

    Split preval blog post method into separate file

--- data/blog-posts.js
+++ data/blog-posts.js
@@ -13,6 +13,7 @@ export type OldBlogPost = {|
 export type NewBlogPost = {|
   title: string,
   summary?: string,
+
   image?: string,
   publishedAt: string,
   published: boolean,
@@ -21,28 +22,8 @@ export type NewBlogPost = {|
 
 export type BlogPost = $Exact<OldBlogPost> | $Exact<NewBlogPost>;
 
-// Load all posts meta data
 const posts: Array<NewBlogPost> = preval`
-  const fs = require('fs')
-  const META = /export\\s+const\\s+meta\\s+=\\s+(\\{(\\n|.)*?\\n\\})/;
-  const files = fs.readdirSync('./pages/thoughts')
-    .filter(file => file.endsWith('.md') || file.endsWith('.mdx'));
-
-  module.exports = files
-    .map(file => {
-      const contents = fs.readFileSync('./pages/thoughts/' + file, 'utf8')
-      const match = META.exec(contents);
-      if (!match || typeof match[1] !== 'string') throw new Error(\`\${file} needs to export const meta = {}\`);
-
-      const meta = eval('(' + match[1] + ')')
-
-      return {
-        ...meta,
-        path: '/thoughts/' + file.replace(/\\.mdx?$/, '')
-      };
-    })
-    .filter(meta => meta.published)
-    .sort((a, b) => new Date(b.publishedAt) - new Date(a.publishedAt));
+  module.exports = require('./get-blog-posts.js');
 `;
 
 module.exports = posts;
