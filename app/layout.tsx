d9988989ca699263697af750c5ef7e1ab60ec91d
Max Stoiber <contact@mxstbr.com>
2024-05-08 07:28:48 -0700

    Inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import './global.css'
 import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
+import { Inter } from 'next/font/google'
 import { Navbar } from './components/nav'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
@@ -12,15 +11,17 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'Max Stoiber',
+    template: '%s | Max Stoiber',
   },
-  description: 'This is my portfolio.',
+  description:
+    'CEO & co-founder of Stellate, creator of styled-components and react-boilerplate and angel investor in early-stage startups.',
   openGraph: {
-    title: 'My Portfolio',
-    description: 'This is my portfolio.',
+    title: 'Max Stoiber',
+    description:
+      'CEO & co-founder of Stellate, creator of styled-components and react-boilerplate and angel investor in early-stage startups.',
     url: baseUrl,
-    siteName: 'My Portfolio',
+    siteName: 'Max Stoiber',
     locale: 'en_US',
     type: 'website',
   },
@@ -39,6 +40,8 @@ export const metadata: Metadata = {
 
 const cx = (...classes) => classes.filter(Boolean).join(' ')
 
+const inter = Inter({ subsets: ['latin'] })
+
 export default function RootLayout({
   children,
 }: {
@@ -49,8 +52,7 @@ export default function RootLayout({
       lang="en"
       className={cx(
         'text-black bg-white dark:text-white dark:bg-black',
-        GeistSans.variable,
-        GeistMono.variable
+        inter.className
       )}
     >
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
