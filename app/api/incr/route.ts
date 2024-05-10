d22178e56992b394ca629cb590fa3d187b26ac95
Max Stoiber <contact@mxstbr.com>
2024-05-10 13:47:49 -0700

    waitUntil

--- app/api/incr/route.ts
+++ app/api/incr/route.ts
@@ -1,4 +1,5 @@
 import { Redis } from '@upstash/redis'
+import { waitUntil } from '@vercel/functions'
 import { NextRequest, NextResponse } from 'next/server'
 
 const redis = Redis.fromEnv()
@@ -12,25 +13,30 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
     return new NextResponse('Slug not found', { status: 400 })
   }
 
-  // Hash the IP and turn it into a hex string
-  const buf = await crypto.subtle.digest(
-    'SHA-256',
-    new TextEncoder().encode(req.ip)
+  waitUntil(
+    new Promise<void>(async (resolve) => {
+      // Hash the IP and turn it into a hex string
+      const buf = await crypto.subtle.digest(
+        'SHA-256',
+        // NOTE: req.ip doesn't work as expected in non-edge runtime.
+        new TextEncoder().encode(req.ip)
+      )
+      const ip = Array.from(new Uint8Array(buf))
+        .map((b) => b.toString(16).padStart(2, '0'))
+        .join('')
+
+      const isNew = await redis.set(['deduplicate', ip, slug].join(':'), true, {
+        nx: true,
+        ex: 24 * 60 * 60,
+      })
+
+      if (isNew) {
+        await redis.incr(['pageviews', 'essay', slug].join(':'))
+      }
+
+      resolve()
+    })
   )
-  const ip = Array.from(new Uint8Array(buf))
-    .map((b) => b.toString(16).padStart(2, '0'))
-    .join('')
-  console.log({ ip })
-
-  const isNew = await redis.set(['deduplicate', ip, slug].join(':'), true, {
-    nx: true,
-    ex: 24 * 60 * 60,
-  })
-  console.log({ isNew })
-
-  if (isNew) {
-    await redis.incr(['pageviews', 'essay', slug].join(':'))
-  }
 
   return new NextResponse(null, { status: 202 })
 }
