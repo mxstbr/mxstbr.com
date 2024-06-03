917e4b138a9a155c92caa5d948950168f34fdf4f
Max Stoiber <contact@mxstbr.com>
2024-06-03 16:37:48 -0400

    update notes

--- app/data/notes.tsx
+++ app/data/notes.tsx
@@ -2,42 +2,52 @@
 
 export const notes = [
   {
-    updatedAt: '2024-05-24T15:33',
+    updatedAt: '2024-06-03T16:28',
     name: '1-on-1 (template)',
     href: 'https://mxstbr.com/notes/1-on-1+(template)',
   },
   {
-    updatedAt: '2024-05-24T15:43',
+    updatedAt: '2024-06-03T16:28',
     name: 'Architectural Decision Record (ADR) Template',
     href: 'https://mxstbr.com/notes/Architectural+Decision+Record+(ADR)+Template',
   },
   {
-    updatedAt: '2024-05-20T07:32',
+    updatedAt: '2024-06-03T16:28',
     name: 'Developer tools are different than tools for any other profession',
     href: 'https://mxstbr.com/notes/Developer+tools+are+different+than+tools+for+any+other+profession',
   },
   {
-    updatedAt: '2024-05-20T07:31',
+    updatedAt: '2024-06-03T16:28',
     name: 'Developer tools startups are almost impossible',
     href: 'https://mxstbr.com/notes/Developer+tools+startups+are+almost+impossible',
   },
   {
-    updatedAt: '2024-05-24T15:33',
+    updatedAt: '2024-06-03T16:27',
+    name: 'Do checkin questions at the beginning of every meeting to encourage connection',
+    href: 'https://mxstbr.com/notes/Do+checkin+questions+at+the+beginning+of+every+meeting+to+encourage+connection',
+  },
+  {
+    updatedAt: '2024-06-03T16:27',
     name: 'How I think about 1-on-1s',
     href: 'https://mxstbr.com/notes/How+I+think+about+1-on-1s',
   },
   {
-    updatedAt: '2024-05-24T15:05',
+    updatedAt: '2024-06-03T16:27',
     name: 'Kernel of good strategy (Template)',
     href: 'https://mxstbr.com/notes/Kernel+of+good+strategy+(Template)',
   },
   {
-    updatedAt: '2024-05-24T15:51',
+    updatedAt: '2024-06-03T16:27',
     name: 'Recurring meeting notes (Template)',
     href: 'https://mxstbr.com/notes/Recurring+meeting+notes+(Template)',
   },
   {
-    updatedAt: '2024-05-20T08:23',
+    updatedAt: '2024-06-03T16:27',
+    name: 'Thoughts on using Obsidian for notes',
+    href: 'https://mxstbr.com/notes/Thoughts+on+using+Obsidian+for+notes',
+  },
+  {
+    updatedAt: '2024-06-03T16:27',
     name: 'Welcome to my notes!',
     href: 'https://mxstbr.com/notes/Welcome+to+my+notes!',
   },
