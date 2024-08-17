eca44181d42187c8a21557797c9a2718c05089a1
Max Stoiber <contact@mxstbr.com>
2024-08-17 13:52:19 -0700

    * feat(mdx-components.tsx): add support for relative links starting with './' in CustomLink function

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -29,7 +29,7 @@ function Table({ data }) {
 function CustomLink(props) {
   let href = props.href
 
-  if (href.startsWith('/')) {
+  if (href.startsWith('/') || href.startsWith('./')) {
     return (
       <Link href={href} {...props}>
         {props.children}
