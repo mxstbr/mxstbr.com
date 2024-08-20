c25a2cff31a14ea7393343e73ce8c9ba08ae08ca
Max Stoiber <contact@mxstbr.com>
2024-08-20 11:12:06 -0700

    * fix(page.tsx): remove redundant phrase in WritingPage text content

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -43,8 +43,8 @@ export default async function WritingPage() {
           <Link href="/notes/digital-garden">digital garden</Link>, my
           collection of explorations. Some of these ideas are still developing
           and I'm actively tending to them; some others are more developed but
-          simply too small to be a standalone <Link href="/">essay</Link>; some
-          others yet I just want be able to reference and link to.{' '}
+          too small to be a standalone <Link href="/">essay</Link>; some others
+          yet I just want be able to reference and link to.{' '}
           <mark>I only write down ideas that strongly resonate with me</mark>,{' '}
           ones that surprise me or influence my thinking in some major way.
         </p>
