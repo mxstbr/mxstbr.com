37d8a6dede678bdec61af1b0e62d9b24b162255b
Max Stoiber <contact@mxstbr.com>
2025-08-20 18:07:09 -0700

    add track record

--- app/investing/investments.ts
+++ app/investing/investments.ts
@@ -3,126 +3,151 @@ export const investments = [
     name: 'Fly.io',
     description: 'a developer-focused cloud for deploying apps close to users',
     href: 'https://fly.io',
-    // subtext: 'Invested in ???, last raised a $70M Series C from EQT Ventures',
+    subtext: 'Series A -> $70M Series C led by EQT Ventures',
   },
 
   {
     name: 'Incident.io',
     description: 'the #1 platform for incident management',
     href: 'https://incident.io',
+    // https://incident.io/blog/incident.io-raises-62m
+    subtext: 'Seed -> $62M Series B led by Insight Partners',
   },
 
   {
     name: 'Raycast',
     description: 'an extensible MacOS launcher',
     href: 'https://raycast.com',
+    // https://techcrunch.com/2024/09/25/raycast-raises-30m-to-bring-its-mac-productivity-app-to-windows-and-ios/
+    subtext: 'Series A -> $30M Series B led by Atomico',
   },
 
   {
     name: 'DX',
     description: 'the developer experience platform',
     href: 'https://getdx.com',
+    subtext: 'Seed',
   },
 
   {
     name: 'Clerk',
     description: 'a complete suite to authenticate and manage users',
     href: 'https://clerk.com',
+    //https://clerk.com/blog/series-b
+    subtext: 'Seed -> $30M Series B led by CRV'
   },
 
   {
     name: 'Upstash',
     description: 'a data platform for the serverless world',
     href: 'https://upstash.com',
+    // https://techcrunch.com/2024/02/08/upstashs-serverless-data-platform-hits-arr-of-1m-just-two-years-after-seed-funding/
+    subtext: 'Seed -> $10M Series A led by a16z'
   },
 
   {
     name: 'Zed',
     description: 'a high-performance, multiplayer code editor',
     href: 'https://zed.dev',
+    // https://x.com/zeddotdev/status/1958174635779903909
+    subtext: 'Seed -> $32M Series B led by Sequoia',
   },
 
   {
     name: 'Liveblocks',
     description: 'a toolchain to build real-time collaborative products',
     href: 'https://liveblocks.io',
+    subtext: 'Seed',
   },
 
   {
     name: 'Arcjet',
     description: 'a security layer for Node.js',
     href: 'https://arcjet.com',
+    subtext: 'Seed'
   },
 
   {
     name: 'Remote First Capital',
     description: 'a fund by Andreas Klinger',
     href: 'https://remotefirstcapital.com',
+    subtext: 'Fund II'
   },
 
   {
     name: 'Magic Bell',
     description: 'an embeddable notification system',
     href: 'https://magicbell.com',
+    subtext: 'Seed'
   },
 
   {
     name: 'DevJobs',
     description: 'a job matching platform for developers',
     href: 'https://devjobs.at',
+    subtext: 'Seed'
   },
 
   {
     name: 'Outsmartly',
     description: 'a CDN for performantly personalizing websites',
     href: 'https://outsmartly.com',
+    subtext: 'Series A'
   },
 
   {
     name: 'Storytell.ai',
     description: 'an AI tool to extract signal from noise',
     href: 'https://storytell.ai',
+    subtext: 'Seed'
   },
 
   {
     name: 'Campsite (acq. by Notion)',
     description: 'an all-in-one team communication app',
     href: 'https://www.notion.com/blog/welcoming-campsites-founders-to-the-notion-team',
+    subtext: 'Seed'
   },
 
   {
     name: 'Fig (acq. by AWS)',
     description: 'a terminal for the 21st century',
     href: 'https://fig.io',
+    subtext: 'Seed'
   },
 
   {
     name: 'StackShare (acq. by Fossa)',
     description: 'a system of record for tech stacks',
     href: 'https://stackshare.io/posts/stackshare-is-now-part-of-fossa',
+    subtext: 'Series A'
   },
 
   {
     name: 'Muse (acq. by bootstrapper)',
     description: 'a tool for thought for deep work',
     href: 'https://museapp.com',
+    subtext: 'Seed'
   },
 
   {
     name: 'Abbot (sunset)',
     description: 'a copilot for customer success in Slack',
     href: 'https://ab.bot',
+    subtext: 'Seed'
   },
 
   {
     name: 'Along (sunset)',
     description: 'an online video platform focussed on creators',
     href: 'https://along.video',
+    subtext: 'Seed'
   },
 
   {
     name: 'Rome (sunset)',
     description: 'a modern JavaScript toolchain',
     href: 'https://rome.tools',
+    subtext: 'Seed'
   },
 ]
